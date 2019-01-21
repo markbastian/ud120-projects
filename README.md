c28b9b45c823fb770ac9bdb360a62f0e06983386
Mark Bastian <markbastian@gmail.com>
2019-01-21 15:26:41 -0700

    Through Lesson 7

--- README.md
+++ README.md
@@ -2,3 +2,37 @@ ud120-projects
 ==============
 
 Starter project code for students taking Udacity ud120
+
+## Notes
+
+## Classifiers
+
+* Naive Bayes
+* SVM (SVC - C = Classifier)
+* Decision Tree
+  * Ensemble methods
+    * Adaboost
+    * Random Forest
+
+# Lesson 6: Datasets and Questions
+More Data > Fine Tuned Algorithm
+Data Types
+ * Numerical (discrete or continuous?)
+ * Categories/Enums
+ * Time series (date/time stamp)
+ * text
+ 
+# Lesson 7: Regressions + Continuous Supervised Learning
+ * Continuous meaning a continuous output range, not continuously learning
+ * Continuous vs. Discrete
+ 
+ Result is often just a simple line fit (y = mx + b)
+ 
+ reg.predict takes an array
+ reg.coef_ & reg.intercept_
+ reg.score provides r**2
+
+Classification vs. Regression
+Discrete vs. Continuous
+Decision Boundary vs. Best Fit Line
+Accuracy vs. Sum of Squares/R^2 to determine accuracy
--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,44 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+# len(enron_data)
+
+import re
+
+re.match(r"LAY", "LAY, KENNETH")
+filter(lambda n : re.match(r"LAY", n), enron_data.keys())
+
+len(filter(lambda (k, v) : v["poi"] == 1, enron_data.items()))
+
+for k,v in enron_data.items():
+    if re.match(r"LAY|SKILLING|FASTOW", k):
+        print(k + ':\t' + str(v['total_payments']))
+
+salaries = 0
+
+for k, v in enron_data.items():
+    if v['salary'] != 'NaN':
+        salaries += 1
+
+emails = 0
+
+for k, v in enron_data.items():
+    if v['email_address'] != 'NaN':
+        emails += 1
+
+payments = 0
+
+for k, v in enron_data.items():
+    if v['total_payments'] != 'NaN':
+        payments += 1
+
+for k, v in enron_data.items():
+    if v['total_payments'] == 'NaN' and v['poi'] == True:
+        payments += 1
+
+len(filter(lambda (k, v) : v['poi'] == True, enron_data.items()))
+
+len(filter(lambda (k, v) : v['total_payments'] == 'NaN' and v['poi'] == True, enron_data.items()))
+
+len(filter(lambda (k, v) : v['total_payments'] == 'NaN', enron_data.items()))
 
